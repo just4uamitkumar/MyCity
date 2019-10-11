4f28ccc7a807c2c0bf6c1da4fc28469f76740c96
just4uamitkumar <just4uamitkumar@gmail.com>
2019-10-11 10:30:20 +0530

    version 1.0.0

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose
 app.use('/api/items', items);
 
 //Serve static assets if in production
-if (process.env.NODE_ENG === 'production') {
+if (process.env.NODE_ENV === 'production') {
     //Set static folder
     app.use(express.static('client/build'));
 
